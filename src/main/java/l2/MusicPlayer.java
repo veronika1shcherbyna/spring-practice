ae01501524c228102c94a7eee1b5482bf9e8d2bd
Veronika <koraf9712@gmail.com>
2023-01-07 17:08:53 +0100

    Dependency injection - setter

--- src/main/java/l2/MusicPlayer.java
+++ src/main/java/l2/MusicPlayer.java
@@ -1,9 +1,35 @@
 package l2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
 
     public MusicPlayer(Music music){
+        this.music = music;
+    }
+    public MusicPlayer() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
